170a9ad1aac9a0a1b8f3a3a4989fcd58b7fe5f7f
parsa-tahavori <113055869+parsa-tahavori@users.noreply.github.com>
2023-01-16 19:16:04 +0000

    Changes in gameScene

--- js/gameScene.js
+++ js/gameScene.js
@@ -13,43 +13,43 @@ class GameScene extends Phaser.Scene {
   /**
    * create an shigaraki
    */
-  createShigaraki() {
-    const shigarakiYLocation = Math.floor(Math.random() * 1080) + 1; // this will get a number between 1 and 1080
-    let shigarakiXVelocity = Math.floor(1 * 920); // this will get a number between 1 and 50
-    shigarakiXVelocity *= Math.round(Math.random()) ? -1 : -1; // this will add minus sign in 50% of cases
-    const aShigaraki = this.physics.add.sprite(
+  createTom() {
+    const TomYLocation = Math.floor(Math.random() * 1080) + 1 // this will get a number between 1 and 1080
+    let TomXVelocity = Math.floor(1 * 920) // this will get a number between 1 and 50
+    TomXVelocity *= Math.round(Math.random()) ? -1 : -1 // this will add minus sign in 50% of cases
+    const aTom = this.physics.add.sprite(
       1920,
-      shigarakiYLocation,
-      "shigaraki"
-    );
-    aShigaraki.body.velocity.y = 30;
-    aShigaraki.body.velocity.x = shigarakiXVelocity;
-    this.shigarakiGroup.add(aShigaraki);
+      TomYLocation,
+      "Tom"
+    )
+    aTom.body.velocity.y = 30
+    aTom.body.velocity.x = TomXVelocity
+    this.TomGroup.add(aTom)
   }
 
   /**
    * This method is the constructor
    */
   constructor() {
-    super({ key: "gameScene" });
+    super({ key: "gameScene" })
 
-    this.background = null;
-    this.background2 = null;
-    this.bakugou = null;
-    this.fireExplosion = false;
-    this.score = 0;
-    this.scoreText = null;
+    this.background = null
+    this.background2 = null
+    this.Jerry = null
+    this.Rope = false
+    this.score = 0
+    this.scoreText = null
 
     this.scoreTextStyle = {
       font: "65px Ariel",
       fill: "#ffffff",
       align: "center",
-    };
+    }
     this.gameOverTextStyle = {
       font: "65px Ariel",
       fill: "#ff0000",
       align: "center",
-    };
+    }
   }
 
   /**
@@ -59,7 +59,7 @@ class GameScene extends Phaser.Scene {
    * @param {object} data - Any data via ScenePlugin.add() or ScenePlugin.start().
    */
   init(data) {
-    this.cameras.main.setBackgroundColor("#ffffff");
+    this.cameras.main.setBackgroundColor("#ffffff")
   }
 
   /**
@@ -67,15 +67,15 @@ class GameScene extends Phaser.Scene {
    * Use it to load assets.
    */
   preload() {
-    console.log("Game Scene");
+    console.log("Game Scene")
     //images
-    this.load.image("startbackground", "./assets/menu-background.png");
-    this.load.image("Jerry", "./assets/Jerry.png");
-    this.load.image("rope", "./assets/rope.png");
-    this.load.image("shigaraki", "./assets/shig.png");
+    this.load.image("startbackground", "./assets/menu-background.png")
+    this.load.image("Jerry", "./assets/Jerrytest.png")
+    this.load.image("mousetrap", "./assets/Mousetrap.png")
+    this.load.image("Tom", "./assets/Tom.png")
     // sound
-    this.load.audio("die", "./assets/dieaudio.mp3");
-    this.load.audio("bomb", "./assets/explosion.mp3");
+    this.load.audio("Scream", "./assets/Scream.mp3")
+    this.load.audio("Throw", "./assets/Throw.mp3")
   }
 
   /**
@@ -84,52 +84,52 @@ class GameScene extends Phaser.Scene {
    * @param {object} data - Any data via ScenePlugin.add() or ScenePlugin.start().
    */
   create(data) {
-    this.background = this.add.image(0, 0, "startbackground").setScale(2.0);
-    this.background.setOrigin(0, 0);
+    this.background = this.add.image(0, 0, "startbackground").setScale(2.0)
+    this.background.setOrigin(0, 0)
     this.background2 = this.add
       .image(1900 * 2, 0, "startbackground")
-      .setScale(2.0);
-    this.background2.setOrigin(0, 0);
+      .setScale(2.0)
+    this.background2.setOrigin(0, 0)
 
     this.scoreText = this.add.text(
       100,
       100,
       "Score: " + this.score.toString(),
       this.scoreTextStyle
-    );
+    )
 
-    this.bakugou = this.physics.add.sprite(1920 / 6, 1080 - 200, "bakugou");
+    this.Jerry = this.physics.add.sprite(1920 / 6, 1080 - 200, "Jerry")
 
-    // create a group for the explosions
-    this.explosionGroup = this.physics.add.group();
+    // create a group for the Rope
+    this.RopeGroup = this.physics.add.group()
 
-    // create a group for the shigarakis
-    this.shigarakiGroup = this.add.group();
-    this.createShigaraki();
+    // create a group for the Tom
+    this.TomGroup = this.add.group()
+    this.createTom()
 
-    //Collisions between explosions and shigarakis
+    //Collisions between Rope and Tom
     this.physics.add.collider(
-      this.explosionGroup,
-      this.shigarakiGroup,
-      function (explosionCollide, shigarakiCollide) {
-        shigarakiCollide.destroy();
-        explosionCollide.destroy();
-        this.score = this.score + 1;
-        this.scoreText.setText("Score: " + this.score.toString());
-        this.createShigaraki();
-        this.createShigaraki();
+      this.RopeGroup,
+      this.TomGroup,
+      function (RopeCollide, TomCollide) {
+        TomCollide.destroy()
+        RopeCollide.destroy()
+        this.score = this.score + 1
+        this.scoreText.setText("Score: " + this.score.toString())
+        this.createTom()
+        this.createTom()
       }.bind(this)
-    );
+    )
 
-    // Collisions between bakugou and shigarakis
+    // Collisions between Jerry and Tom
     this.physics.add.collider(
-      this.bakugou,
-      this.shigarakiGroup,
-      function (bakugouCollide, shigarakiCollide) {
-        this.sound.play("die");
-        this.physics.pause();
-        shigarakiCollide.destroy();
-        bakugouCollide.destroy();
+      this.Jerry,
+      this.TomGroup,
+      function (JerryCollide, TomCollide) {
+        this.sound.play("Scream")
+        this.physics.pause()
+        TomCollide.destroy()
+        JerryCollide.destroy()
         this.gameOverText = this.add
           .text(
             1920 / 2,
@@ -137,13 +137,11 @@ class GameScene extends Phaser.Scene {
             "Ha you lost, loser!\nClick to try again.",
             this.gameOverTextStyle
           )
-          .setOrigin(0.5);
-        this.gameOverText.setInteractive({ userHandCursor: true });
-        this.gameOverText.on("pointerdown", () =>
-          this.scene.start("gameScene")
-        );
+          .setOrigin(0.5)
+        this.gameOverText.setInteractive({ userHandCursor: true })
+        this.gameOverText.on("pointerdown", () => this.scene.start("gameScene"))
       }.bind(this)
-    );
+    )
   }
 
   /**
@@ -155,63 +153,63 @@ class GameScene extends Phaser.Scene {
   update(time, delta) {
     // called 60 times a second, hopefully!
 
-    const keyUpObj = this.input.keyboard.addKey("UP");
-    const keyDownObj = this.input.keyboard.addKey("DOWN");
-    const keySpaceObj = this.input.keyboard.addKey("SPACE");
+    const keyUpObj = this.input.keyboard.addKey("UP")
+    const keyDownObj = this.input.keyboard.addKey("DOWN")
+    const keySpaceObj = this.input.keyboard.addKey("SPACE")
 
     // move background each tick
     if (this.background.x >= -1900 * 2) {
-      this.background.x = this.background.x - 2;
+      this.background.x = this.background.x - 2
     } else {
-      console.log("move background1");
-      this.background.x = 1900 * 2;
+      console.log("move background1")
+      this.background.x = 1900 * 2
     }
     if (this.background2.x >= -1900 * 2) {
-      this.background2.x = this.background2.x - 2;
+      this.background2.x = this.background2.x - 2
     } else {
-      console.log("move background2");
-      this.background2.x = 1900 * 2;
+      console.log("move background2")
+      this.background2.x = 1900 * 2
     }
 
     if (keyUpObj.isDown === true) {
-      this.bakugou.y -= 15;
-      if (this.bakugou.y < 0) {
-        this.bakugou.y = 0;
+      this.Jerry.y -= 15
+      if (this.Jerry.y < 0) {
+        this.Jerry.y = 0
       }
     }
 
     // Moves the character down
     if (keyDownObj.isDown === true) {
-      this.bakugou.y += 15;
-      if (this.bakugou.y > 1080) {
-        this.bakugou.y = 1080;
+      this.Jerry.y += 15
+      if (this.Jerry.y > 1080) {
+        this.Jerry.y = 1080
       }
     }
 
     if (keySpaceObj.isDown === true) {
       if (this.fireExplosion === false) {
-        // fire explosion
-        this.fireExplosion = true;
-        const aNewexplosion = this.physics.add.sprite(
-          this.bakugou.x,
-          this.bakugou.y,
-          "explosion"
-        );
-        this.explosionGroup.add(aNewexplosion);
-        this.sound.play("bomb");
+        // fire Rope
+        this.fireExplosion = true
+        const aNewRope = this.physics.add.sprite(
+          this.Jerry.x,
+          this.Jerry.y,
+          "mousetrap"
+        )
+        this.RopeGroup.add(aNewRope)
+        this.sound.play("Throw")
       }
     }
 
     if (keySpaceObj.isUp === true) {
-      this.fireExplosion = false;
+      this.fireExplosion = false
     }
 
-    this.explosionGroup.children.each(function (item) {
-      item.x = item.x + 15;
+    this.RopeGroup.children.each(function (item) {
+      item.x = item.x + 15
       if (item.x < 0) {
-        item.destroy();
+        item.destroy()
       }
-    });
+    })
   }
 }
-export default GameScene;
+export default GameScene
